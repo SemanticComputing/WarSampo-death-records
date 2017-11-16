6524aca67d5df801a7dc74399ee613b206d2ae1f
Erkki Heino <erkki.heino@aalto.fi>
2017-11-16 16:22:35 +0200

    Use short urls in links

--- casualties/filters.js
+++ casualties/filters.js
@@ -2,9 +2,15 @@
     'use strict';
 
     angular.module('facetApp')
+    /* @ngInject */
     .filter('castArray', function(_) {
         return function(input) {
             return _.castArray(input);
+        };
+    })
+    .filter('localId', function() {
+        return function(input) {
+            return input.replace(new RegExp('^.*/(.*?)$'), '$1');
         };
     });
 })();
